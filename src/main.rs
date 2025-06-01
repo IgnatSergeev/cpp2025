69f70e007bc2f92246c17347586ea4010032a6d4
IgnatSergeev <ignat.sergeev@softcom.su>
2025-06-02 01:20:35 +0300

    export mode

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,10 @@
 mod runners;
 mod tasks;
+mod mode;
 
-use clap::{ Parser, ValueEnum };
-use std::fmt;
+use clap::Parser;
 use std::time::Instant;
-
-#[derive(Clone, ValueEnum)]
-enum Mode {
-    Serial,
-    Parallel,
-    Fibonacci,
-}
-
-impl fmt::Display for Mode {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Mode::Serial => write!(f, "Serial"),
-            Mode::Parallel=> write!(f, "Parallel"),
-            Mode::Fibonacci => write!(f, "Fibonacci"),
-        }
-    }
-}
+use mode::Mode;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -42,33 +26,40 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    println!("Running in {} mode", args.benchmark);
+    let mut tasks_map = std::collections::HashMap::new();
+
+    tasks_map.insert(
+        "dummy".to_string(),
+        tasks::dummy_task as fn()
+    );
+
+    println!("Starting {} spawn benchmark", args.benchmark);
     println!("Configuration: {} threads, {} tasks", args.threads.unwrap_or(num_cpus::get()), args.tasks);
 
     match args.benchmark {
         Mode::Serial => {
-            std::println!("Starting serial spawn of dummy io");
+            for task in tasks_map {
+                std::println!("\nRunning {} function benchmark", task.0);
+                let start = Instant::now();
+                runners::serial(args.threads.unwrap_or(num_cpus::get()), args.tasks, task.1);
+                let end = Instant::now();
 
-            let start = Instant::now();
-            runners::serial(args.threads.unwrap_or(num_cpus::get()), args.tasks, tasks::dummy_io_task);
-            let end = Instant::now();
-
-            let time = end - start;
-            std::println!("Throughput: {} tasks/s", args.tasks as f32 / time.as_secs_f32());
-            std::println!("Elapsed time {}s", time.as_secs_f32());
-
+                let time = end - start;
+                std::println!("Throughput: {} tasks/s", args.tasks as f32 / time.as_secs_f32());
+                std::println!("Elapsed time {}s", time.as_secs_f32());
+            }
         }
         Mode::Parallel => {
-            std::println!("Starting parallel spawn of dummy io");
+            for task in tasks_map {
+                std::println!("\nRunning {} function benchmark", task.0);
+                let start = Instant::now();
+                runners::parallel(args.threads.unwrap_or(num_cpus::get()), args.tasks, task.1);
+                let end = Instant::now();
 
-            let start = Instant::now();
-            runners::parallel(args.threads.unwrap_or(num_cpus::get()), args.tasks, tasks::dummy_io_task);
-            let end = Instant::now();
-
-            let time = end - start;
-            std::println!("Throughput: {} tasks/s", args.tasks as f32 / time.as_secs_f32());
-            std::println!("Elapsed time {}s", time.as_secs_f32());
-
+                let time = end - start;
+                std::println!("Throughput: {} tasks/s", args.tasks as f32 / time.as_secs_f32());
+                std::println!("Elapsed time {}s", time.as_secs_f32());
+            }
         }
         Mode::Fibonacci => {
             println!("Fibonacci");
---
+++ src/mode.rs
@@ -0,0 +1,19 @@
+use std::fmt;
+use clap::ValueEnum;
+
+#[derive(Clone, ValueEnum)]
+pub enum Mode {
+    Serial,
+    Parallel,
+    Fibonacci,
+}
+
+impl fmt::Display for Mode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Mode::Serial => write!(f, "Serial"),
+            Mode::Parallel=> write!(f, "Parallel"),
+            Mode::Fibonacci => write!(f, "Fibonacci"),
+        }
+    }
+}
